01cae7adc61c65947e4826674e1b00559d9736d1
Brent Williams <jbwtech@gmail.com>
2021-09-11 12:11:50 -0400

    mods for elsint, prettier, and ejs

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require("express");
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express :)");
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (req, res) => {
+  res.render('pages/index', { pageTitle: 'Welcome' });
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
