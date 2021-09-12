bf101fd46d15ef4ab272010499cf5332bba73c1d
Brent Williams <jbwtech@gmail.com>
2021-09-11 20:41:34 -0400

    templates

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
 
 const routes = require('./routes');
 
@@ -8,12 +15,27 @@ const app = express();
 
 const port = 3000;
 
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['klaerdvmnakdsrf', 'kaeriujkvcnakesr'],
+  })
+);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
