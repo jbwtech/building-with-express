004a91ed29d4022f5730068240364181cef5609b
Brent Williams <jbwtech@gmail.com>
2021-09-15 14:07:12 -0400

    Lesson series 4

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,17 @@ const feedbackRoute = require('./feedback');
 const router = express.Router();
 
 module.exports = (params) => {
-  router.get('/', (req, res) => {
-    res.render('layout', { pageTitle: 'Welcome', template: 'index' });
+  const { speakersService } = params;
+
+  router.get('/', async (req, res) => {
+    const artwork = await speakersService.getAllArtwork();
+    const topSpeakers = await speakersService.getList();
+    res.render('layout', {
+      pageTitle: 'Welcome',
+      template: 'index',
+      topSpeakers,
+      artwork,
+    });
   });
 
   router.use('/speakers', speakersRoute(params));
